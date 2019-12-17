cb31f7ce3daa6def93d83ea0dfe47e136f5f715f
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-12-17 16:24:24 -0500

    PrivateRoute component built

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -4,7 +4,16 @@ import { Route, Redirect } from "react-router-dom";
 const PrivateRoute = ({ components: Component, ...rest }) => {
     return (
         <Route
-        
+        {...rest}
+        render={props => {
+            if (localStorage.getItem("token")){
+                return <Component {...props} /> 
+            } else {
+                return <Redirect to="/login" />;
+            }
+        }}
         />
     )
 }
+
+export default PrivateRoute;
